df8ecfc0821c867af0dea225433ea9f435f7077c
Aravind A <arvd00013@gmail.com>
2025-08-16 20:54:48 +0530

    Add files via upload

---
+++ Namah_Shivaya_with_userinput_name.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name;
+
+    //User input for name
+    cout << "Enter your name: ";
+    cin >> name;
+
+    //Prints Namah Shivaya with the name given by the user
+    cout << "Namah Shivaya, " << name << "!";
+
+    return 0;
+}
